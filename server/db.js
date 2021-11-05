5c7753876bc7a13e18ab7f81a28c3b8016572bdc
Aman Kumar <amankumar02740@gmail.com>
2021-11-05 22:10:12 +0530

    add git ignore in client

--- server/db.js
+++ server/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 
-const mongoURI = "mongodb://localhost:27017/ashberry?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+ const mongoAT = "mongodb+srv://aman:Kumar@321@cluster0.veuzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
+    mongoose.connect(mongoAT,()=>{
         console.log("connnected to Mongo Successfully")
     })
 
 }
 
 module.exports = connectToMongo;
+
+
+
+
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var cors = require('cors')
 connectToMongo();
 const app = express()
-const port = 5000
+const port = 6000
 
 
 app.use(cors())
