f7bfd07af8d4abf5a1134c2681d70e6d5bba6322
Ashish Thakur <sendashishthakur@gmail.com>
2021-11-06 09:14:09 +0530

    added mongo URI

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-
+const mongoAt = "mongodb+srv://Ash1:Ash1@cluster0.pbef3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const mongoURI = "mongodb://localhost:27017/ashberry?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 
 const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
+    mongoose.connect(mongoAt,()=>{
         console.log("connnected to Mongo Successfully")
     })
 
