59c4cc99dff38e0148dde472edc88dc2ae3646f6
Ashish Thakur <sendashishthakur@gmail.com>
2021-11-06 10:41:16 +0530

    update in mongoose connect

--- server/db.js
+++ server/db.js
@@ -5,10 +5,13 @@ const mongoAt = "mongodb+srv://Ash1:Ash1@cluster0.pbef3.mongodb.net/myFirstDatab
 const mongoURI = "mongodb://localhost:27017/ashberry?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 const mongoAT1 = "mongodb+srv://aman:Kumar@321@cluster0.veuzb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const connectToMongo = ()=>{
-    mongoose.connect(mongoAt,()=>{
-
-        console.log("connnected to Mongo Successfully")
-    })
+    mongoose.connect(mongoAt,{
+        dbName: 'Ashberry',
+        userNewUrlParse : true,
+        useUnifiedTopology: true
+    }).then(()=> {
+        console.log("connnected to MongoDB Successfully")
+    });
 
 }
 
